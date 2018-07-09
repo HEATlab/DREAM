45d93c94ad9fba876b84b13a1ac174f70f00bcff
Jordan R A <jabrahams@hmc.edu>
2018-07-09 10:30:58 -0700

    Small edit to lower multirun runtime

--- bin/multirun_script.bash
+++ bin/multirun_script.bash
@@ -1,22 +1,22 @@
 #!/bin/bash
-nice -10 ~/libheat/bin/rb-simulate -s 150 -e early -t 60 -o ~/data/20180707/20180707_early.csv ~/sim_files/*/original_1.json ~/sim_files/*/original_2.json
-nice -10 ~/libheat/bin/rb-simulate -s 150 -e srea -t 60 -o ~/data/20180707/20180707_srea.csv ~/sim_files/*/original_1.json ~/sim_files/*/original_2.json
-nice -10 ~/libheat/bin/rb-simulate -s 150 -e drea -t 60 -o ~/data/20180707/20180707_drea.csv ~/sim_files/*/original_1.json ~/sim_files/*/original_2.json
+#nice -10 ~/libheat/bin/rb-simulate -s 50 -e early -t 60 -o ~/data/20180707/20180707_early.csv ~/sim_files/*/original_1.json ~/sim_files/*/original_2.json
+#nice -10 ~/libheat/bin/rb-simulate -s 50 -e srea -t 60 -o ~/data/20180707/20180707_srea.csv ~/sim_files/*/original_1.json ~/sim_files/*/original_2.json
+nice -10 ~/libheat/bin/rb-simulate -s 50 -e drea -t 60 -o ~/data/20180707/20180707_drea.csv ~/sim_files/*/original_1.json ~/sim_files/*/original_2.json
 
-nice -10 ~/libheat/bin/rb-simulate -s 150 -e drea-si -t 60 --threshold 0.0 -o ~/data/20180707/20180707_drea_si_0_0.csv ~/sim_files/*/original_1.json ~/sim_files/*/original_2.json
-nice -10 ~/libheat/bin/rb-simulate -s 150 -e drea-si -t 60 --threshold 0.25 -o ~/data/20180707/20180707_drea_si_0_3.csv ~/sim_files/*/original_1.json ~/sim_files/*/original_2.json
-nice -10 ~/libheat/bin/rb-simulate -s 150 -e drea-si -t 60 --threshold 0.5 -o ~/data/20180707/20180707_drea_si_0_5.csv ~/sim_files/*/original_1.json ~/sim_files/*/original_2.json
-nice -10 ~/libheat/bin/rb-simulate -s 150 -e drea-si -t 60 --threshold 0.75 -o ~/data/20180707/20180707_drea_si_0_7.csv ~/sim_files/*/original_1.json ~/sim_files/*/original_2.json
-nice -10 ~/libheat/bin/rb-simulate -s 150 -e drea-si -t 60 --threshold 1.0 -o ~/data/20180707/20180707_drea_si_1_0.csv ~/sim_files/*/original_1.json ~/sim_files/*/original_2.json
+nice -10 ~/libheat/bin/rb-simulate -s 50 -e drea-si -t 60 --threshold 0.0 -o ~/data/20180707/20180707_drea_si_0_0.csv ~/sim_files/*/original_1.json ~/sim_files/*/original_2.json
+nice -10 ~/libheat/bin/rb-simulate -s 50 -e drea-si -t 60 --threshold 0.25 -o ~/data/20180707/20180707_drea_si_0_3.csv ~/sim_files/*/original_1.json ~/sim_files/*/original_2.json
+nice -10 ~/libheat/bin/rb-simulate -s 50 -e drea-si -t 60 --threshold 0.5 -o ~/data/20180707/20180707_drea_si_0_5.csv ~/sim_files/*/original_1.json ~/sim_files/*/original_2.json
+nice -10 ~/libheat/bin/rb-simulate -s 50 -e drea-si -t 60 --threshold 0.75 -o ~/data/20180707/20180707_drea_si_0_7.csv ~/sim_files/*/original_1.json ~/sim_files/*/original_2.json
+nice -10 ~/libheat/bin/rb-simulate -s 50 -e drea-si -t 60 --threshold 1.0 -o ~/data/20180707/20180707_drea_si_1_0.csv ~/sim_files/*/original_1.json ~/sim_files/*/original_2.json
 
-nice -10 ~/libheat/bin/rb-simulate -s 150 -e drea-alp -t 60 --threshold 0.0 -o ~/data/20180707/20180707_drea_alp_0_0.csv ~/sim_files/*/original_1.json ~/sim_files/*/original_2.json
-nice -10 ~/libheat/bin/rb-simulate -s 150 -e drea-alp -t 60 --threshold 0.25 -o ~/data/20180707/20180707_drea_alp_0_3.csv ~/sim_files/*/original_1.json ~/sim_files/*/original_2.json
-nice -10 ~/libheat/bin/rb-simulate -s 150 -e drea-alp -t 60 --threshold 0.5 -o ~/data/20180707/20180707_drea_alp_0_5.csv ~/sim_files/*/original_1.json ~/sim_files/*/original_2.json
-nice -10 ~/libheat/bin/rb-simulate -s 150 -e drea-alp -t 60 --threshold 0.75 -o ~/data/20180707/20180707_drea_alp_0_7.csv ~/sim_files/*/original_1.json ~/sim_files/*/original_2.json
-nice -10 ~/libheat/bin/rb-simulate -s 150 -e drea-alp -t 60 --threshold 1.0 -o ~/data/20180707/20180707_drea_alp_1_0.csv ~/sim_files/*/original_1.json ~/sim_files/*/original_2.json
+nice -10 ~/libheat/bin/rb-simulate -s 50 -e drea-alp -t 60 --threshold 0.0 -o ~/data/20180707/20180707_drea_alp_0_0.csv ~/sim_files/*/original_1.json ~/sim_files/*/original_2.json
+nice -10 ~/libheat/bin/rb-simulate -s 50 -e drea-alp -t 60 --threshold 0.25 -o ~/data/20180707/20180707_drea_alp_0_3.csv ~/sim_files/*/original_1.json ~/sim_files/*/original_2.json
+nice -10 ~/libheat/bin/rb-simulate -s 50 -e drea-alp -t 60 --threshold 0.5 -o ~/data/20180707/20180707_drea_alp_0_5.csv ~/sim_files/*/original_1.json ~/sim_files/*/original_2.json
+nice -10 ~/libheat/bin/rb-simulate -s 50 -e drea-alp -t 60 --threshold 0.75 -o ~/data/20180707/20180707_drea_alp_0_7.csv ~/sim_files/*/original_1.json ~/sim_files/*/original_2.json
+nice -10 ~/libheat/bin/rb-simulate -s 50 -e drea-alp -t 60 --threshold 1.0 -o ~/data/20180707/20180707_drea_alp_1_0.csv ~/sim_files/*/original_1.json ~/sim_files/*/original_2.json
 
-nice -10 ~/libheat/bin/rb-simulate -s 150 -e drea-ar -t 60 --threshold 0.0 -o ~/data/20180707/20180707_drea_ar_0_0.csv ~/sim_files/*/original_1.json ~/sim_files/*/original_2.json
-nice -10 ~/libheat/bin/rb-simulate -s 150 -e drea-ar -t 60 --threshold 0.25 -o ~/data/20180707/20180707_drea_ar_0_3.csv ~/sim_files/*/original_1.json ~/sim_files/*/original_2.json
-nice -10 ~/libheat/bin/rb-simulate -s 150 -e drea-ar -t 60 --threshold 0.5 -o ~/data/20180707/20180707_drea_ar_0_5.csv ~/sim_files/*/original_1.json ~/sim_files/*/original_2.json
-nice -10 ~/libheat/bin/rb-simulate -s 150 -e drea-ar -t 60 --threshold 0.75 -o ~/data/20180707/20180707_drea_ar_0_7.csv ~/sim_files/*/original_1.json ~/sim_files/*/original_2.json
-nice -10 ~/libheat/bin/rb-simulate -s 150 -e drea-ar -t 60 --threshold 1.0 -o ~/data/20180707/20180707_drea_ar_1_0.csv ~/sim_files/*/original_1.json ~/sim_files/*/original_2.json
+nice -10 ~/libheat/bin/rb-simulate -s 50 -e drea-ar -t 60 --threshold 0.0 -o ~/data/20180707/20180707_drea_ar_0_0.csv ~/sim_files/*/original_1.json ~/sim_files/*/original_2.json
+nice -10 ~/libheat/bin/rb-simulate -s 50 -e drea-ar -t 60 --threshold 0.25 -o ~/data/20180707/20180707_drea_ar_0_3.csv ~/sim_files/*/original_1.json ~/sim_files/*/original_2.json
+nice -10 ~/libheat/bin/rb-simulate -s 50 -e drea-ar -t 60 --threshold 0.5 -o ~/data/20180707/20180707_drea_ar_0_5.csv ~/sim_files/*/original_1.json ~/sim_files/*/original_2.json
+nice -10 ~/libheat/bin/rb-simulate -s 50 -e drea-ar -t 60 --threshold 0.75 -o ~/data/20180707/20180707_drea_ar_0_7.csv ~/sim_files/*/original_1.json ~/sim_files/*/original_2.json
+nice -10 ~/libheat/bin/rb-simulate -s 50 -e drea-ar -t 60 --threshold 1.0 -o ~/data/20180707/20180707_drea_ar_1_0.csv ~/sim_files/*/original_1.json ~/sim_files/*/original_2.json
