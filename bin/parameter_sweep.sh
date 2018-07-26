a17740d7023f69cb3e4b3575f5f2b9036212d0c0
Jordan R A <jabrahams@hmc.edu>
2018-07-26 13:34:57 -0700

    Update parameter sweep to current date, to run on knuth

--- bin/parameter_sweep.sh
+++ bin/parameter_sweep.sh
@@ -5,17 +5,18 @@
 #
 # Author: Jordan R. Abrahams
 # Date: 2018-07-14
+# Updated Last: 2018-07-26
 
 for ar in 0.0 0.0625 0.125 0.25 0.5 1.0;
 do
-  for si in 0.0 0.0625 0.125 0.25 0.5 1.0;
+  for sc in 0.0 0.0625 0.125 0.25 0.5 1.0;
   do
     ar_name=$(echo ${ar} | tr '.' '_' )
-    si_name=$(echo ${si} | tr '.' '_' )
+    sc_name=$(echo ${sc} | tr '.' '_' )
     nice -10 pstn-simulate -s 100 -e arsi -t 60 \
         --ar-threshold "$ar" \
-        --si-threshold "$si" \
-        -o "$HOME/data/20180714/20180714_arsi_${ar_name}__${si_name}.csv" \
+        --si-threshold "$sc" \
+        -o "$HOME/data/20180726/20180726_dream_${ar_name}__${sc_name}.csv" \
         "$HOME/sim_files"
   done
 done
