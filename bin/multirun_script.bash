11db86d060b823900b6b503d424453632bfdfc52
Jordan R A <jabrahams@hmc.edu>
2019-03-09 14:11:07 -0800

    Add new multirun shell scripts and modifications

--- bin/multirun_script.bash
+++ bin/multirun_script.bash
@@ -15,14 +15,14 @@
 #nice -10 pstn-simulate -s 100 -e drea-si -t 60 --si-threshold 1.0 -o ~/data/20180719/20180719_drea_si_1_0.csv ~/sim_files
 
 
-nice -10 pstn-simulate -s 100 -e drea-ara -t 60 --ar-threshold 0.0 -o ~/data/20180719/20180719_drea_ara_0_0.csv ~/sim_files
-nice -10 pstn-simulate -s 100 -e drea-ara -t 60 --ar-threshold 0.0625 -o ~/data/20180719/20180719_drea_ara_0_0625.csv ~/sim_files
-nice -10 pstn-simulate -s 100 -e drea-ara -t 60 --ar-threshold 0.125 -o ~/data/20180719/20180719_drea_ara_0_125.csv ~/sim_files
-nice -10 pstn-simulate -s 100 -e drea-ara -t 60 --ar-threshold 0.25 -o ~/data/20180719/20180719_drea_ara_0_25.csv ~/sim_files
-nice -10 pstn-simulate -s 100 -e drea-ara -t 60 --ar-threshold 0.375 -o ~/data/20180719/20180719_drea_ara_0_375.csv ~/sim_files
-nice -10 pstn-simulate -s 100 -e drea-ara -t 60 --ar-threshold 0.5 -o ~/data/20180719/20180719_drea_ara_0_5.csv ~/sim_files
-nice -10 pstn-simulate -s 100 -e drea-ara -t 60 --ar-threshold 0.75 -o ~/data/20180719/20180719_drea_ara_0_75.csv ~/sim_files
-nice -10 pstn-simulate -s 100 -e drea-ara -t 60 --ar-threshold 1.0 -o ~/data/20180719/20180719_drea_ara_1_0.csv ~/sim_files
+#nice -10 pstn-simulate -s 100 -e drea-ara -t 60 --ar-threshold 0.0 -o ~/data/20180719/20180719_drea_ara_0_0.csv ~/sim_files
+#nice -10 pstn-simulate -s 100 -e drea-ara -t 60 --ar-threshold 0.0625 -o ~/data/20180719/20180719_drea_ara_0_0625.csv ~/sim_files
+#nice -10 pstn-simulate -s 100 -e drea-ara -t 60 --ar-threshold 0.125 -o ~/data/20180719/20180719_drea_ara_0_125.csv ~/sim_files
+#nice -10 pstn-simulate -s 100 -e drea-ara -t 60 --ar-threshold 0.25 -o ~/data/20180719/20180719_drea_ara_0_25.csv ~/sim_files
+#nice -10 pstn-simulate -s 100 -e drea-ara -t 60 --ar-threshold 0.375 -o ~/data/20180719/20180719_drea_ara_0_375.csv ~/sim_files
+#nice -10 pstn-simulate -s 100 -e drea-ara -t 60 --ar-threshold 0.5 -o ~/data/20180719/20180719_drea_ara_0_5.csv ~/sim_files
+#nice -10 pstn-simulate -s 100 -e drea-ara -t 60 --ar-threshold 0.75 -o ~/data/20180719/20180719_drea_ara_0_75.csv ~/sim_files
+#nice -10 pstn-simulate -s 100 -e drea-ara -t 60 --ar-threshold 1.0 -o ~/data/20180719/20180719_drea_ara_1_0.csv ~/sim_files
 
 #nice -10 pstn-simulate -s 100 -e drea-ar -t 60 --ar-threshold 0.0 -o ~/data/20180719/20180719_drea_ar_0_0625.csv ~/sim_files
 #nice -10 pstn-simulate -s 100 -e drea-ar -t 60 --ar-threshold 0.0625 -o ~/data/20180719/20180719_drea_ar_0_0625.csv ~/sim_files
@@ -32,3 +32,8 @@ nice -10 pstn-simulate -s 100 -e drea-ara -t 60 --ar-threshold 1.0 -o ~/data/201
 #nice -10 pstn-simulate -s 100 -e drea-ar -t 60 --ar-threshold 0.5 -o ~/data/20180719/20180719_drea_ar_0_5.csv ~/sim_files
 #nice -10 pstn-simulate -s 100 -e drea-ar -t 60 --ar-threshold 0.75 -o ~/data/20180719/20180719_drea_ar_0_75.csv ~/sim_files
 #nice -10 pstn-simulate -s 100 -e drea-ar -t 60 --ar-threshold 1.0 -o ~/data/20180719/20180719_drea_ar_1_0.csv ~/sim_files
+
+nice -10 pstn-simulate -s 60 -e arsi -t 60 --seed 1454079265 --ar-threshold 0.5 --si-threshold 0.0 --start-point 4 --stop-point 5 -o $HOME/data/20181118/20181118_dream_0_5__0_0.csv --mit-parse $HOME/mit_dataset/rover_coordination.json
+nice -10 pstn-simulate -s 60 -e arsi -t 60 --seed 1454079265 --ar-threshold 0.75 --si-threshold 0.0625 --stop-point 20 -o $HOME/data/20181118/20181118_dream_0_75__0_0625.csv --mit-parse $HOME/mit_dataset/rover_coordination.json
+nice -10 pstn-simulate -s 60 -e arsi -t 60 --seed 1454079265 --ar-threshold 1.0 --si-threshold 0.0625 --stop-point 20 -o $HOME/data/20181118/20181118_dream_1_0__0_0625.csv --mit-parse $HOME/mit_dataset/rover_coordination.json
+nice -10 pstn-simulate -s 60 -e arsi -t 60 --seed 1454079265 --ar-threshold 0.75 --si-threshold 0.0 --start-point 20 -o $HOME/data/20181118/20181118_dream_0_75__0_0.csv --mit-parse $HOME/mit_dataset/rover_coordination.json
--- bin/parameter_sweep.sh
+++ bin/parameter_sweep.sh
@@ -5,7 +5,7 @@
 #
 # Author: Jordan R. Abrahams
 # Date: 2018-07-14
-# Updated Last: 2018-07-26
+# Updated Last: 2018-11-17
 
 for ar in 0.0 0.0625 0.125 0.25 0.5 1.0;
 do
@@ -13,10 +13,11 @@ do
   do
     ar_name=$(echo ${ar} | tr '.' '_' )
     sc_name=$(echo ${sc} | tr '.' '_' )
-    nice -10 pstn-simulate -s 100 -e arsi -t 60 \
+    nice -15 pstn-simulate -s 20 -e arsi -t 60 \
         --ar-threshold "$ar" \
         --si-threshold "$sc" \
-        -o "$HOME/data/20180726/20180726_dream_${ar_name}__${sc_name}.csv" \
-        "$HOME/sim_files"
+        -o "$HOME/data/20181117/20181117_mit_dream_${ar_name}__${sc_name}.csv" \
+        --mit-parse --stop-point 19 \
+        "$HOME/mit_dataset/rover_coordination.json"
   done
 done
